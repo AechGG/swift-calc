07866c965362328bcc619ee26eb95c84644c7b22
AechGG <hazg@me.com>
2020-08-28 14:56:23 +0100

    feat: set up launch screen and print nums

--- swift-calc/ViewController.swift
+++ swift-calc/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isFinishedTypingNumber: Bool = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true;
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let number = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = number;
+                isFinishedTypingNumber = false;
+            } else {
+                displayLabel.text = displayLabel.text! + number;
+            }
+        }
+        
+    }
 }
 
